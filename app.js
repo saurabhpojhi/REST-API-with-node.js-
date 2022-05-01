1b53ff0e079dfc189728158b2d8d38e4d76c6dba
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-02 00:22:23 +0530

    add

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 
-//const productRoutes = require('./api/routes/products');
+app.use(require('./routes/products'))
 
-//app.use('/products',productRoutes);
-
-app.use((req,res,next)=>{
-    res.status(200).json({
-        message:'it works '
-    })
-});
+// app.use((req,res,next)=>{
+//     res.status(200).json({
+//         message:'it works '
+//     })
+// });
 
 module.exports =app;
--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+
 router.get('/',(req,res,next)=>{
     res.status(200).json({
         message:'handle Get request to produts'
