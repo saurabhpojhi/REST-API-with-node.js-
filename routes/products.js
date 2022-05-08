0e13ff138d514fa917a72ed2b66606d21afbcad1
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-08 16:26:50 +0530

    products

--- routes/products.js
+++ routes/products.js
@@ -1,45 +1,55 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const router = express.Router();
 
+const Product = require('../models/product');
 
-router.get('/',(req,res,next)=>{
+router.get('/', (req, res, next) => {
     res.status(200).json({
-        message:'handle Get request to produts'
+        message: 'handle Get request to produts'
     });
 });
 
-router.post('/',(req,res,next)=>{
-    const product = {
+router.post('/', (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
+    });
+    product.save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err)
+        });
     res.status(200).json({
-        message:'created product'
+        message: 'created product successfully'
     });
 });
 
-router.get('/:productsId',(req,res,next)=>{
+router.get('/:productsId', (req, res, next) => {
     const id = req.params.productId;
-    if(id==='special'){
+    if (id === 'special') {
         res.status(200).json({
-            message:'You discoverd the special ID'
+            message: 'You discoverd the special ID'
         });
-    }else{
+    } else {
         res.status(200).json({
-            message:'You passed ID'
+            message: 'You passed ID'
         });
     }
 })
 
-router.patch('/:productsId',(req,res,next)=>{
-        res.status(200).json({
-            message:'Update Product'
-        });
+router.patch('/:productsId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Update Product'
+    });
 })
 
-router.delete('/:productsId',(req,res,next)=>{
+router.delete('/:productsId', (req, res, next) => {
     res.status(200).json({
-        message:'Delete Product'
+        message: 'Delete Product'
     });
 })
 module.exports = router;
